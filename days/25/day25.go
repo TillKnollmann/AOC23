4277dd34598cff194b16a3ef3a895b508b248e2d
Till Knollmann <till-knollmann@outlook.de>
2023-12-23 19:31:09 +0100

    Added skeletons for days 22 to 25

---
+++ days/25/day25.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+const DAY = "25"
+
+func Part1(input string, maxSteps int) string {
+
+	content := GetContent(input)
+
+	return string(content[0])
+}
+
+func Part2(input string) string {
+
+	content := GetContent(input)
+
+	return string(content[0])
+}
+
+func GetContent(filepath string) string {
+
+	content, err := os.ReadFile(filepath)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(content)
+}
+
+func main() {
+
+	fmt.Println(fmt.Sprintf("Part 1: %s", Part1(fmt.Sprintf("input/%s/in.txt", DAY), 64)))
+	fmt.Println(fmt.Sprintf("Part 2: %s", Part2(fmt.Sprintf("input/%s/in.txt", DAY))))
+}
